b356b13b3f41601ba628761c4a62a9f4d1a7f961
Ihita <2021csb1095@iitrpr.ac.in>
2023-07-10 03:39:25 +0530

    Final computation

--- solver.h
+++ solver.h
@@ -1,19 +1,3 @@
-#ifndef SOLVER_H
-#define SOLVER_H
+#include "helper.h"
 
-#include <stdio.h>
-#include <math.h>
-
-const int N; //number of variables
-const char dash = '-';
-int str_len;
-int valid_minterms[];
-
-//m is the minterm number (0 to 2^(N-1))
-//n is N
-int num_ones(int m, int n);
-
-//will convert final array of chars ('0' '1' and '-') to array of abc chars
-char* string(char* a);
-
-#endif
+struct BucketStore find_implicants(list minterms);
